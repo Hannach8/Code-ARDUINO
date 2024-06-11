bfdbc118250c961691611091ed293330cd38b97f
Hannach8 <hamzahannach891@gmail.com>
2024-06-11 12:12:19 +0100

    Update code_arduino.ino

--- code_arduino.ino
+++ code_arduino.ino
@@ -1,134 +1,115 @@
-#include<LiquidCrystal.h>
+#include <LiquidCrystal.h>
 
 const int rs = 4, en = 5, d4 = 6, d5 = 7, d6 = 8, d7 = 9;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int capteurHumidty = A0, capteurlum = A1, nvDeauBas = 0, nvDeauHaut = 1, Pompe = 2, Led = 13, buzzer = 12, vanne = 3;
-bool bas, haut;
+const int capteurHumidty = A0, capteurlum = A1, capteurNiveauEau = 10;
+const int Pompe = 2, LedRed = 13, buzzer = 12, vanne = 3, LedBleue = 11;
 
 int humidity, analoghumidity;
 int lux, analoglux;
+bool niveauEau;
 
-void setup(){
-  
- 
-  pinMode(nvDeauBas, INPUT);
-  pinMode(nvDeauHaut, INPUT);
+void setup() {
+  Serial.begin(9600);
+  pinMode(capteurNiveauEau, INPUT);
   pinMode(Pompe, OUTPUT);
-  pinMode(Led, OUTPUT);
+  pinMode(LedRed, OUTPUT);
   pinMode(vanne, OUTPUT);
   pinMode(buzzer, OUTPUT);
+  pinMode(LedBleue, OUTPUT);
 
-  lcd.begin(16, 4);
-  lcd.setCursor(0, 0);
-  lcd.print("EST BM (systeme");
-  lcd.setCursor(0, 1);
-  lcd.print("d'irrigation");
-  lcd.setCursor(0,2);
-  lcd.print("automatique)");
-  delay(1500);
- 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("realiser par :");
-  delay(700);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("ATIFI mohamed");
-  lcd.setCursor(0, 1);
-  lcd.print("MARAOUI Ayoub");
-  lcd.setCursor(0, 2);
-  lcd.print("KHOULANE MOUSSA");
-  delay(1000);
+  lcd.begin(16, 2);
   
-  }
+  // Afficher les crédits
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Realise par :");
+  delay(1500);
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("MARAOUI Ibrahim");
+  delay(1500);
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("HANNACH Hamza");
+  delay(1500);
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("LAMRABET Marouane");
+  delay(1500);
+  
+  lcd.clear();
+}
+
 void loop() {
-
-  bas = digitalRead(nvDeauBas);
-  haut = digitalRead(nvDeauHaut);
-
+  // Lire les valeurs du capteur d'humidité et vérifier les lectures
   analoghumidity = analogRead(capteurHumidty);
+  Serial.print("Valeur du capteur d'humidite: ");
+  Serial.println(analoghumidity);
 
+  // Mapper les valeurs lues en pourcentage d'humidité
   humidity = map(analoghumidity, 1023, 0, 100, 0);
 
+  // Lire les valeurs du capteur de lumière et les mapper
   analoglux = analogRead(capteurlum);
-
   lux = map(analoglux, 9600, 0, 100, 0);
 
-  if (!haut == HIGH) {
-    if (bas == HIGH) {
-      digitalWrite(vanne, HIGH);
-      tone(buzzer, 3000);
-      lcd.clear();
-      lcd.setCursor(0, 1);
-      lcd.print("humidity ");
-      lcd.print(humidity);
-      lcd.print(" %");
-      lcd.setCursor(0, 2);
-      lcd.print("niveau bas");
-      delay(300);
-    }
-    else {
-      noTone(buzzer);
-      lcd.clear();
-      lcd.setCursor(0, 1);
-      lcd.print("humidity ");
-      lcd.print(humidity);
-      lcd.print(" %");
-      lcd.setCursor(0, 2);
-      lcd.print("niveau moyen :");
-      delay(300);
-    }
-  }
-  if (!bas == HIGH) {
-    if (haut == HIGH) {
-      digitalWrite(vanne, LOW);
-      lcd.clear();
-      lcd.setCursor(0, 1);
-      lcd.print("humidity ");
-      lcd.print(humidity);
-      lcd.print(" %");
-      lcd.setCursor(0, 2);
-      lcd.print("niveau haut");
-      delay(200);
-    }
-    else {
-      lcd.clear();
-      lcd.setCursor(0, 1);
-      lcd.print("humidity ");
-      lcd.print(humidity);
-      lcd.print(" %");
-      lcd.setCursor(0, 2);
-      lcd.print("niveau moyen");
-      delay(200);
-    }
+  // Lire l'état du capteur de niveau d'eau
+  niveauEau = digitalRead(capteurNiveauEau);
+
+  // Logique de contrôle pour l'humidité et la lumière
+  if (humidity >= 30 && lux <= 1) {
+    digitalWrite(Pompe, HIGH);
+    digitalWrite(LedRed, HIGH);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pompe: arroser");
+    lcd.setCursor(0, 1);
+    lcd.print("humidite ");
+    lcd.print(humidity);
+    lcd.print(" %");
+    delay(2000); // Augmenter le délai ici pour une durée d'affichage plus longue
+  } else {
+    digitalWrite(Pompe, LOW);
+    digitalWrite(LedRed, LOW);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Pompe: arret");
+    lcd.setCursor(0, 1);
+    lcd.print("humidite ");
+    lcd.print(humidity);
+    lcd.print(" %");
+    delay(2000); // Augmenter le délai ici pour une durée d'affichage plus longue
   }
 
-  if (humidity <= 30) {
-    if (lux <= 1) {
-      digitalWrite(Pompe, HIGH);
-      digitalWrite(Led, HIGH);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Pompe: arroser");
-      lcd.setCursor(0, 1);
-      lcd.print("humidity ");
-      lcd.print(humidity);
-      lcd.print(" %");
-      delay(200);
-    }
+  // Logique de contrôle pour le niveau d'eau
+  if (!niveauEau) { // Niveau d'eau bas
+    digitalWrite(vanne, HIGH);
+    digitalWrite(LedBleue, HIGH);
+    tone(buzzer, 3000);
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print("niveau bas");
+    delay(3000); // Augmenter le délai ici pour une durée d'affichage plus longue
+  } else { // Niveau d'eau haut
+    digitalWrite(vanne, LOW);
+    digitalWrite(LedBleue, LOW);
+    noTone(buzzer);
   }
-  if (humidity > 30)   {
-      digitalWrite(Pompe, LOW);
-      digitalWrite(Led, LOW);
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Pompe: arret");
-      lcd.setCursor(0, 1);
-      lcd.print("humidity ");
-      lcd.print(humidity);
-      lcd.print(" %");
-  }
-  delay(300);
 
+  // Afficher les valeurs sur l'écran LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("humidite ");
+  lcd.print(humidity);
+  lcd.print(" %");
+  lcd.setCursor(0, 1);
+  lcd.print("luminosite ");
+  lcd.print(lux);
+  lcd.print(" %");
+  delay(3000); // Augmenter le délai ici pour une durée d'affichage plus longue
 }
